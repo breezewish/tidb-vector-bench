19486dd12f6da2bd28e5f9a2153f317420244033
Wish <breezewish@outlook.com>
2024-07-29 22:11:29 +0800

    Support displaying recall
    Signed-off-by: Wish <breezewish@outlook.com>

--- main.py
+++ main.py
@@ -95,11 +95,11 @@ class Sample(peewee.Model):
         database = mysql_db
         db_table = "sample"
 
-    id = peewee.CharField(
+    id = UnsignedIntegerField(
         # primary_key=True,
     )
     vec = VectorField(
-        constraints=[peewee.SQL("COMMENT 'hnsw(distance=cosine)'")],
+        constraints=[peewee.SQL("COMMENT 'hnsw(distance=l2)'")],
     )
 
 
@@ -107,11 +107,12 @@ print(
     f"+ Connecting to {mysql_db.connect_params['user']}@{mysql_db.connect_params['host']}..."
 )
 mysql_db.connect()
-print("+ Creating tables...")
-mysql_db.create_tables([Sample])
 
 
 def run_load():
+    print("+ Creating tables...")
+    mysql_db.create_tables([Sample])
+
     with h5py.File("./fashion-mnist-784-euclidean.hdf5", "r") as data_file:
         data: numpy.ndarray = data_file["train"][()]
         data_with_id = [(idx, data[idx]) for idx in range(0, len(data))]
@@ -120,7 +121,7 @@ def run_load():
             print(f"  - Insert PK: {batch[0][0]}")
             Sample.insert_many(batch, fields=[Sample.id, Sample.vec]).execute()
 
-    print("done")
+    print("+ Done")
 
 
 def run_test():
@@ -129,22 +130,53 @@ def run_test():
         f"SELECT ROWS_STABLE_INDEXED, ROWS_STABLE_NOT_INDEXED, ROWS_DELTA_NOT_INDEXED FROM INFORMATION_SCHEMA.TIFLASH_INDEXES WHERE TIDB_TABLE='sample'"
     )
     print(
-        tabulate.tabulate(cursor.fetchall(), headers=["Indexed", "NotIndexed", "Delta"])
+        tabulate.tabulate(
+            cursor.fetchall(),
+            headers=["Indexed", "NotIndexed", "Delta"],
+            tablefmt="psql",
+        )
     )
 
     print("+ Querying...")
     with h5py.File("./fashion-mnist-784-euclidean.hdf5", "r") as data_file:
-        query_row: numpy.ndarray = data_file["test"][0]
-        cursor = mysql_db.execute_sql(
-            "EXPLAIN ANALYZE SELECT id, VEC_Cosine_Distance(vec, %s) AS d FROM sample ORDER BY d LIMIT 10",
+        test_rowid = 0
+
+        query_row: numpy.ndarray = data_file["test"][test_rowid]
+        groundtruth_results_set = set(data_file["neighbors"][test_rowid])
+
+        # First query: select id, to calculate a recall rate.
+        with mysql_db.execute_sql(
+            "SELECT id FROM sample ORDER BY VEC_L2_Distance(vec, %s) LIMIT 100",
             (format_vec(query_row),),
-        )
-        print(tabulate.tabulate(cursor.fetchall()))
+        ) as cursor:
+            actual_results = cursor.fetchall()
+            actual_results_set = set([int(row[0]) for row in actual_results])
+            recall = (
+                len(groundtruth_results_set & actual_results_set)
+                / len(groundtruth_results_set)
+                * 100
+            )
+            print(f"Recall@100: {recall:.2f}%")
+
+        # Second query: Use EXPLAIN ANALYZE to check the (hot cache) performance.
+        with mysql_db.execute_sql(
+            "EXPLAIN ANALYZE SELECT id, VEC_L2_Distance(vec, %s) AS d FROM sample ORDER BY d LIMIT 100",
+            (format_vec(query_row),),
+        ) as cursor:
+            print(tabulate.tabulate(cursor.fetchall(), tablefmt="psql"))
+
+
+def run_clean():
+    print("+ Dropping tables...")
+    mysql_db.drop_tables([Sample])
+    print("+ Done")
 
 
 if action == "load":
     run_load()
 elif action == "test":
     run_test()
+elif action == "clean":
+    run_clean()
 else:
     exit_print_usage()
